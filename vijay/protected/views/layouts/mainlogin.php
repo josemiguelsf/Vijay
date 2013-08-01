4c59a6afb31150259e39c0e2c6b8f01ff1eb754e
josemiguelsf <josemiguelsf@gmail.com>
2013-08-01 08:31:25 -0700

    css and text change to front login

--- vijay/protected/views/layouts/mainlogin.php
+++ vijay/protected/views/layouts/mainlogin.php
@@ -48,9 +48,9 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by VJ Ideas.<br/>
 		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
+		<?php //echo Yii::powered(); ?>
 	</div><!-- footer -->
 
 </div><!-- page -->
